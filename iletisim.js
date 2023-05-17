8bffce39c6d7c3387301cb65cfb1277a7fe378d2
AhmetSafa0707 <119118562+AhmetSafa0707@users.noreply.github.com>
2023-05-18 02:00:58 +0300

    Onuncu Commit
    -İletişim sayfasındaki inputlara girilen değerlerin doğruluğunu kontrol eden javascript kodunu ekledim.
    -Eğer inputtaki değerler doğru ise hakkımda.html sayfasına yönlendiriliyor.

---
+++ iletisim.js
@@ -0,0 +1,37 @@
+function validateForm() {
+  var ad = document.getElementById("kullaniciIsmi").value;
+  var soyad = document.getElementById("kullaniciSoyIsmi").value;
+  var mail = document.getElementById("kullaniciMail").value;
+  var sifre = document.getElementById("kullaniciSifre").value;
+
+  var sonucDiv = document.getElementById("sonuc");
+  sonucDiv.innerHTML = ""; // Önceki sonuçları temizle
+
+  // Ad, soyad, mail ve şifre alanlarının boş olup olmadığını kontrol et
+  if (ad == "" || soyad == "" || mail == "" || sifre == "") {
+    sonucDiv.innerHTML = "Lütfen tüm alanları doldurun.";
+    return;
+  }
+
+  // E-posta formatını kontrol et
+  var emailRegex = /^[a-zA-Z0-9._-]+@sakarya\.edu\.tr$/;
+  if (!emailRegex.test(mail)) {
+    sonucDiv.innerHTML = "Geçerli bir sakarya.edu.tr e-posta adresi girin.";
+    return;
+  }
+
+var nameRegex = /^[a-zA-ZğüşıöçĞÜŞİÖÇ\s]+$/;
+  if (!nameRegex.test(ad) || !nameRegex.test(soyad)) {
+    sonucDiv.innerHTML = "Ad ve soyad alanlarına sadece harf girin.";
+    return;
+  }
+
+  // Şifre en az 6 karakter olmalı
+  if (sifre.length < 6) {
+    sonucDiv.innerHTML = "Şifre en az 6 karakterden oluşmalıdır.";
+    return;
+  }
+
+  // Tüm kontroller başarılı ise sayfaya yönlendir
+   window.location.href = "hakkimda.html";
+}
